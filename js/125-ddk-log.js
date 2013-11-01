0d9b491b2793464fe91495940e941e3139438426
John Madhavan-Reese <jsmreese@pureshare.com>
2013-11-01 14:33:32 -0400

    Updated DDK logging to use new ddk.log flag.

--- js/125-ddk-log.js
+++ js/125-ddk-log.js
@@ -6,7 +6,8 @@
  *
  * Creates DDK versions of standard console methods for logging information to the client console.
  * Will log an arbitrary number of arguments, just like console.log().
- * Will log only when DDK.DEBUG is true (usually set via URL query string parameter &ddk.debug=true).
+ * Always outputs logs for DDK.error, DDK.warn, and DDK.info.
+ * Will output logs from DDK.log only when DDK_LOG is true (usually set via URL query string parameter &ddk.log=true).
  * Will not throw an error when console is undefined (IE 8 and 9 when Developer Tools is not open).
  * Will JSON.stringify objects in older console implementations (and recent IE) to avoid logging "[object Object]".
  */
@@ -42,13 +43,13 @@ _.each(["error", "warn", "info", "log"], function(method, methodIndex) {
 });
 */
 
-_.each(["error", "warn", "info", "log"], function(method, methodIndex) {
+_.each(["log", "error", "warn", "info"], function(method, methodIndex) {
 	DDK[method] = function() {
 		var func = window.console && window.console[method];
 
-		// if DDK.DEBUG is true, and console and console[method] are defined, pass arguments to the method
-		// DDK.error() will always log unless console.error is undefined
-		if (func && (DDK.DEBUG || !methodIndex)) {
+		// DDK.error, DDK.warn, and DDK.info will always log unless console is not available
+		// if DDK_LOG is true, DDK.log will log
+		if (func && (DDK_LOG || methodIndex)) {
 			if (func.apply) {
 				// if console[method].apply is defined, use it (console methods require the context of console)
 				func.apply(console, arguments);
