5f89c06b4ce107b7eefc5217b2478af8d1c16c7e
John Madhavan-Reese <jsmreese@pureshare.com>
2013-11-01 12:14:16 -0400

    Modify lo-dash string API.

--- js/015-lodash-string.js
+++ js/015-lodash-string.js
@@ -1,11 +1,9 @@
-//  Underscore.string
-//  (c) 2010 Esa-Matti Suuronen <esa-matti aet suuronen dot org>
-//  Underscore.string is freely distributable under the terms of the MIT license.
-//  Documentation: https://github.com/epeli/underscore.string
-//  Some code is borrowed from MooTools and Alexandru Marasteanu.
-//  Version '2.3.0'
-
-!function(root, String){
+// PureShare String Utility Library
+//
+// started as a fork of Underscore.string version 2.3.0
+// (c) 2010 Esa-Matti Suuronen <esa-matti aet suuronen dot org>
+// https://github.com/epeli/underscore.string
+function(root, String){
   'use strict';
 
   // Defining helper functions.
@@ -48,128 +46,6 @@
   var reversedEscapeChars = {};
   for(var key in escapeChars) reversedEscapeChars[escapeChars[key]] = key;
   reversedEscapeChars["'"] = '#39';
-
-  // sprintf() for JavaScript 0.7-beta1
-  // http://www.diveintojavascript.com/projects/javascript-sprintf
-  //
-  // Copyright (c) Alexandru Marasteanu <alexaholic [at) gmail (dot] com>
-  // All rights reserved.
-
-  var sprintf = (function() {
-    function get_type(variable) {
-      return Object.prototype.toString.call(variable).slice(8, -1).toLowerCase();
-    }
-
-    var str_repeat = strRepeat;
-
-    var str_format = function() {
-      if (!str_format.cache.hasOwnProperty(arguments[0])) {
-        str_format.cache[arguments[0]] = str_format.parse(arguments[0]);
-      }
-      return str_format.format.call(null, str_format.cache[arguments[0]], arguments);
-    };
-
-    str_format.format = function(parse_tree, argv) {
-      var cursor = 1, tree_length = parse_tree.length, node_type = '', arg, output = [], i, k, match, pad, pad_character, pad_length;
-      for (i = 0; i < tree_length; i++) {
-        node_type = get_type(parse_tree[i]);
-        if (node_type === 'string') {
-          output.push(parse_tree[i]);
-        }
-        else if (node_type === 'array') {
-          match = parse_tree[i]; // convenience purposes only
-          if (match[2]) { // keyword argument
-            arg = argv[cursor];
-            for (k = 0; k < match[2].length; k++) {
-              if (!arg.hasOwnProperty(match[2][k])) {
-                throw new Error(sprintf('[_.sprintf] property "%s" does not exist', match[2][k]));
-              }
-              arg = arg[match[2][k]];
-            }
-          } else if (match[1]) { // positional argument (explicit)
-            arg = argv[match[1]];
-          }
-          else { // positional argument (implicit)
-            arg = argv[cursor++];
-          }
-
-          if (/[^s]/.test(match[8]) && (get_type(arg) != 'number')) {
-            throw new Error(sprintf('[_.sprintf] expecting number but found %s', get_type(arg)));
-          }
-          switch (match[8]) {
-            case 'b': arg = arg.toString(2); break;
-            case 'c': arg = String.fromCharCode(arg); break;
-            case 'd': arg = parseInt(arg, 10); break;
-            case 'e': arg = match[7] ? arg.toExponential(match[7]) : arg.toExponential(); break;
-            case 'f': arg = match[7] ? parseFloat(arg).toFixed(match[7]) : parseFloat(arg); break;
-            case 'o': arg = arg.toString(8); break;
-            case 's': arg = ((arg = String(arg)) && match[7] ? arg.substring(0, match[7]) : arg); break;
-            case 'u': arg = Math.abs(arg); break;
-            case 'x': arg = arg.toString(16); break;
-            case 'X': arg = arg.toString(16).toUpperCase(); break;
-          }
-          arg = (/[def]/.test(match[8]) && match[3] && arg >= 0 ? '+'+ arg : arg);
-          pad_character = match[4] ? match[4] == '0' ? '0' : match[4].charAt(1) : ' ';
-          pad_length = match[6] - String(arg).length;
-          pad = match[6] ? str_repeat(pad_character, pad_length) : '';
-          output.push(match[5] ? arg + pad : pad + arg);
-        }
-      }
-      return output.join('');
-    };
-
-    str_format.cache = {};
-
-    str_format.parse = function(fmt) {
-      var _fmt = fmt, match = [], parse_tree = [], arg_names = 0;
-      while (_fmt) {
-        if ((match = /^[^\x25]+/.exec(_fmt)) !== null) {
-          parse_tree.push(match[0]);
-        }
-        else if ((match = /^\x25{2}/.exec(_fmt)) !== null) {
-          parse_tree.push('%');
-        }
-        else if ((match = /^\x25(?:([1-9]\d*)\$|\(([^\)]+)\))?(\+)?(0|'[^$])?(-)?(\d+)?(?:\.(\d+))?([b-fosuxX])/.exec(_fmt)) !== null) {
-          if (match[2]) {
-            arg_names |= 1;
-            var field_list = [], replacement_field = match[2], field_match = [];
-            if ((field_match = /^([a-z_][a-z_\d]*)/i.exec(replacement_field)) !== null) {
-              field_list.push(field_match[1]);
-              while ((replacement_field = replacement_field.substring(field_match[0].length)) !== '') {
-                if ((field_match = /^\.([a-z_][a-z_\d]*)/i.exec(replacement_field)) !== null) {
-                  field_list.push(field_match[1]);
-                }
-                else if ((field_match = /^\[(\d+)\]/.exec(replacement_field)) !== null) {
-                  field_list.push(field_match[1]);
-                }
-                else {
-                  throw new Error('[_.sprintf] huh?');
-                }
-              }
-            }
-            else {
-              throw new Error('[_.sprintf] huh?');
-            }
-            match[2] = field_list;
-          }
-          else {
-            arg_names |= 2;
-          }
-          if (arg_names === 3) {
-            throw new Error('[_.sprintf] mixing positional and named placeholders is not (yet) supported');
-          }
-          parse_tree.push(match);
-        }
-        else {
-          throw new Error('[_.sprintf] huh?');
-        }
-        _fmt = _fmt.substring(match[0].length);
-      }
-      return parse_tree;
-    };
-
-    return str_format;
-  })();
 
   var coerceNestedObject = function (object) {
 	_.each(object, function (value, key, obj) {
@@ -236,9 +112,7 @@
   // Defining underscore.string
 
   var _s = {
-
-    VERSION: '2.3.0',
-
+  
     isBlank: function(str){
       if (str == null) str = '';
       return (/^\s*$/).test(str);
@@ -334,7 +208,7 @@
       return _s.splice(str, i, 0, substr);
     },
 
-    include: function(str, needle){
+    contains: function(str, needle){
       if (needle === '') return true;
       if (str == null) return false;
       return String(str).indexOf(needle) !== -1;
@@ -506,13 +380,6 @@
       return _s.pad(str, length, padStr, 'both');
     },
 
-    sprintf: sprintf,
-
-    vsprintf: function(fmt, argv){
-      argv.unshift(fmt);
-      return sprintf.apply(null, argv);
-    },
-
     toNumber: function(str, decimals) {
       if (!str) return 0;
       str = _s.trim(str);
@@ -562,20 +429,14 @@
 
     toSentence: function(array, separator, lastSeparator, serial) {
       separator = separator || ', '
-      lastSeparator = lastSeparator || ' and '
+      lastSeparator = lastSeparator || ', and '
       var a = array.slice(), lastMember = a.pop();
 
       if (array.length > 2 && serial) lastSeparator = _s.rtrim(separator) + lastSeparator;
 
       return a.length ? a.join(separator) + lastSeparator + lastMember : lastMember;
     },
-
-    toSentenceSerial: function() {
-      var args = slice.call(arguments);
-      args[3] = true;
-      return _s.toSentence.apply(_s, args);
-    },
-
+	
     slugify: function(str) {
       if (str == null) return '';
 
@@ -599,17 +460,6 @@
       return _s.surround(str, '"');
     },
 
-    exports: function() {
-      var result = {};
-
-      for (var prop in this) {
-        if (!this.hasOwnProperty(prop) || prop.match(/^(?:include|contains|reverse)$/)) continue;
-        result[prop] = this[prop];
-      }
-
-      return result;
-    },
-
     repeat: function(str, qty, separator){
       if (str == null) return '';
 
@@ -621,36 +471,6 @@
       // this one is about 300x slower in Google Chrome
       for (var repeat = []; qty > 0; repeat[--qty] = str) {}
       return repeat.join(separator);
-    },
-
-    naturalCmp: function(str1, str2){
-      if (str1 == str2) return 0;
-      if (!str1) return -1;
-      if (!str2) return 1;
-
-      var cmpRegex = /(\.\d+)|(\d+)|(\D+)/g,
-        tokens1 = String(str1).toLowerCase().match(cmpRegex),
-        tokens2 = String(str2).toLowerCase().match(cmpRegex),
-        count = Math.min(tokens1.length, tokens2.length);
-
-      for(var i = 0; i < count; i++) {
-        var a = tokens1[i], b = tokens2[i];
-
-        if (a !== b){
-          var num1 = parseInt(a, 10);
-          if (!isNaN(num1)){
-            var num2 = parseInt(b, 10);
-            if (!isNaN(num2) && num1 - num2)
-              return num1 - num2;
-          }
-          return a < b ? -1 : 1;
-        }
-      }
-
-      if (tokens1.length === tokens2.length)
-        return tokens1.length - tokens2.length;
-
-      return str1 < str2 ? -1 : 1;
     },
 
     levenshtein: function(str1, str2) {
@@ -818,16 +638,6 @@
 		
 		return pairs;
 	},
-	
-	// trimWhiteSpace(str)
-	// trims leading and trailing white space from a string
-	// returns the trimmed string
-	trimWhiteSpace: function(str) {
-		// return null if the string is null or undefined
-		if (str == null) { return  null };
-		
-		return str.replace(/^\s*/, "").replace(/\s*$/, "");
-	},
 
 	// coerce(str)
 	// attempts to coerce a string to a native JavaScript type
@@ -854,7 +664,7 @@
 		// return null if the string is null or undefined
 		if (str == null) { return  null };
 		
-		trimmedStr = _s.trimWhiteSpace(str);
+		trimmedStr = _s.trim(str);
 		
 		// call coerceType using the first non-whitspace character of the string
 		// as the lookup in coerceTriggers for the coerceType caseKey
@@ -873,35 +683,13 @@
   // expose coerceType and coerceTriggers as properties of _s.coerece
   _s.coerce.coerceType = coerceType;
   _s.coerce.coerceTriggers = coerceTriggers;
-
-  // Aliases
-
-  _s.strip    = _s.trim;
-  _s.lstrip   = _s.ltrim;
-  _s.rstrip   = _s.rtrim;
-  _s.center   = _s.lrpad;
-  _s.rjust    = _s.lpad;
-  _s.ljust    = _s.rpad;
-  _s.contains = _s.include;
-  _s.q        = _s.quote;
+  
 
   // Exporting
 
-  // CommonJS module is defined
-  if (typeof exports !== 'undefined') {
-    if (typeof module !== 'undefined' && module.exports)
-      module.exports = _s;
-
-    exports._s = _s;
-  }
-
-  // Register as a named module with AMD.
-  if (typeof define === 'function' && define.amd)
-    define('underscore.string', [], function(){ return _s; });
-
-
-  // Integrate with Underscore.js if defined
-  // or create our own underscore object.
+  // create _ object if it is not defined
   root._ = root._ || {};
-  root._.string = _s;
+  
+  // expose via _.string and _.str
+  root._.string = root._.str = _s;
 }(this, String);
