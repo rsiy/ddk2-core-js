88b35941fedbbb2e6a6cd5868996b3e181f4c590
John Madhavan-Reese <jsmreese@pureshare.com>
2013-11-21 13:41:10 -0500

    Added jQuery plugin helpers for DDK Controls.

--- js/025-jQuery-plugins.js
+++ js/025-jQuery-plugins.js
@@ -1,4 +1,58 @@
-$.fn.reverse = [].reverse;
+(function(window, undefined) {
+	var $ = window.jQuery;
+
+	$.fn.reverse = [].reverse;
+
+	/* $.topZ jQuery plugin
+	 * Based on http://abcoder.com/javascript/a-better-process-to-find-maximum-z-index-within-a-page
+	 * Returns one plus the maximum z-index of all elements that are children of the document body.
+	 * by: jsmreese
+	 */
+	$.topZ = function () {
+		return 1 + Math.max.apply(undefined, $.map($("body > *"), function(elem, index) {
+			return parseInt($(elem).css("z-index"), 10) || 1;
+		}));
+	};
+	
+	/* $.fn.parentControl jQuery plugin
+	 * Returns a jQuery object containing the unique set of parent DDK control elements for all elements in the calling context.
+	 * by: jsmreese
+	 */
+	$.fn.parentControl = function () {
+		return this.pushStack($.unique($.map(this, function (elem) {
+			return $(elem).closest("[id^=\"psc_\"][id$=\"_widget\"]").get();
+		})));
+	};
+	
+	/* $.fn.controlData jQuery plugin
+	 * Returns an object or array containing the DDK control data for a control or set of DDK control elements.
+	 * If called on a single control, will return an object.
+	 * If called on a set of controls, will return an array.
+	 * If called on an empty set of controls, will return `null`.
+	 * by: jsmreese
+	 */
+	$.fn.controlData = function () {
+		var ret = _.compact($.map(this, function (elem) {
+			var $elem = $(elem),
+				elemIdParts = elem.id.split("_"),
+				id = elemIdParts[2],
+				name = elemIdParts[1];
+			
+			// return `null` for all non-DDK-control elements
+			if (elemIdParts[0] !== "psc" || elemIdParts[3] !== "widget" || !id || !name ) { return null; }
+			
+			return $.extend(true,
+				{ id: id, name: name },
+				$elem.find("#psc_" + name + "_data_" + id).data(),
+				$elem.find("[data-ddk-metrics]").data()
+			);
+		}));
+		
+		if (!ret.length) { return null; };
+		if (ret.length === 1) { return ret[0]; }
+		return ret;
+	};
+})(this);
 
 /*! http://mths.be/placeholder v2.0.7 by @mathias */
 ;(function(f,h,$){var a='placeholder' in h.createElement('input'),d='placeholder' in h.createElement('textarea'),i=$.fn,c=$.valHooks,k,j;if(a&&d){j=i.placeholder=function(){return this};j.input=j.textarea=true}else{j=i.placeholder=function(){var l=this;l.filter((a?'textarea':':input')+'[placeholder]').not('.placeholder').bind({'focus.placeholder':b,'blur.placeholder':e}).data('placeholder-enabled',true).trigger('blur.placeholder');return l};j.input=a;j.textarea=d;k={get:function(m){var l=$(m);return l.data('placeholder-enabled')&&l.hasClass('placeholder')?'':m.value},set:function(m,n){var l=$(m);if(!l.data('placeholder-enabled')){return m.value=n}if(n==''){m.value=n;if(m!=h.activeElement){e.call(m)}}else{if(l.hasClass('placeholder')){b.call(m,true,n)||(m.value=n)}else{m.value=n}}return l}};a||(c.input=k);d||(c.textarea=k);$(function(){$(h).delegate('form','submit.placeholder',function(){var l=$('.placeholder',this).each(b);setTimeout(function(){l.each(e)},10)})});$(f).bind('beforeunload.placeholder',function(){$('.placeholder').each(function(){this.value=''})})}function g(m){var l={},n=/^jQuery\d+$/;$.each(m.attributes,function(p,o){if(o.specified&&!n.test(o.name)){l[o.name]=o.value}});return l}function b(m,n){var l=this,o=$(l);if(l.value==o.attr('placeholder')&&o.hasClass('placeholder')){if(o.data('placeholder-password')){o=o.hide().next().show().attr('id',o.removeAttr('id').data('placeholder-id'));if(m===true){return o[0].value=n}o.focus()}else{l.value='';o.removeClass('placeholder');l==h.activeElement&&l.select()}}}function e(){var q,l=this,p=$(l),m=p,o=this.id;if(l.value==''){if(l.type=='password'){if(!p.data('placeholder-textinput')){try{q=p.clone().attr({type:'text'})}catch(n){q=$('<input>').attr($.extend(g(this),{type:'text'}))}q.removeAttr('name').data({'placeholder-password':true,'placeholder-id':o}).bind('focus.placeholder',b);p.data({'placeholder-textinput':q,'placeholder-id':o}).before(q)}p=p.removeAttr('id').hide().prev().attr('id',o).show()}p.addClass('placeholder');p[0].value=p.attr('placeholder')}else{p.removeClass('placeholder')}}}(this,document,jQuery));
